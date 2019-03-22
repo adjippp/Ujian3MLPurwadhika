b773d9b9abc8d0c492564a60b2e0cf3537ed02fc
Adji Pratama Permana Putra <42745069+adjippp@users.noreply.github.com>
2019-03-22 16:18:19 +0700

    Add files via upload

--- Ujian_Pemain_Muda_Berbakat/soal2_1.py
+++ Ujian_Pemain_Muda_Berbakat/soal2_1.py
@@ -4,28 +4,28 @@ import matplotlib.pyplot as plt
 
 
 df=pd.read_csv('data.csv')
-df=df.fillna(np.NaN)
+# df=df.fillna(np.NaN)
 #membuat plot antara Age vs Overall
-x=df['Age']
-y=df['Overall']
-y2=df['Potential']
 xAge=df['Age'][(df['Age']<=25)&(df['Overall']>=80)&(df['Potential']>=80)]
 yOverall=df['Overall'][(df['Age']<=25)&(df['Overall']>=80)&(df['Potential']>=80)]
+yPotential = df['Potential'][(df['Age']<=25)&(df['Overall']>=80)&(df['Potential']>=80)]
+indexX=xAge.index.tolist()
+xout=df['Age'].loc[~df.index.isin(indexX)]
+yOvout=df['Overall'].loc[~df.index.isin(indexX)]
+yPotout=df['Potential'].loc[~df.index.isin(indexX)]
 plt.figure()
 plt.subplot(121)
-plt.scatter(x,y,label='Non-Target',color='r')
 plt.scatter(xAge,yOverall,label='Target',color='g')
+plt.scatter(xout,yOvout,label='Non-Target',color='r')
 plt.title('Age vs Overall')
 plt.xlabel('Age')
 plt.ylabel('Overall')
 plt.legend()
 plt.grid(True)
 
-
-yPotential = df['Potential'][(df['Age']<=25)&(df['Overall']>=80)&(df['Potential']>=80)]
 plt.subplot(122)
-plt.scatter(x,y2,label='Non-Target',color='r')
 plt.scatter(xAge,yPotential,label='Target',color='g')
+plt.scatter(xout,yPotout,label='Non-Target',color='r')
 plt.title('Age vs Potential')
 plt.xlabel('Age')
 plt.ylabel('Potential')
